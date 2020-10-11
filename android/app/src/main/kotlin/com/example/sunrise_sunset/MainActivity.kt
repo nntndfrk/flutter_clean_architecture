b64268e1ff3347b6a8a8ef5c213ec06b70a0ade1
nntndfrk <nntndfrk@gmail.com>
2020-10-11 13:42:15 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/sunrise_sunset/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sunrise_sunset
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sunrise_sunset/internal/application.dart';
+
+void main() {
+  runApp(Application());
+}
